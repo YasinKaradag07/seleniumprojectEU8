9057c127ce1e3ff27759624999de8be4d5770e90
YasinKaradag07 <yasinkaradag3607@gmail.com>
2022-04-29 17:05:53 +0300

    driver.navigate().to-back-refresh and getTitle()

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigations {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //1- Set up the browser driver
         WebDriverManager.chromedriver().setup();
@@ -16,6 +16,35 @@ public class BasicNavigations {
 
         //3- Go to tesla.com
         driver.get("https://www.tesla.com");
+
+        String currentTitle = driver.getTitle();
+        System.out.println("currentTitle = " + currentTitle);
+
+        //4- Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        //5- Use selenium to navigate back
+        driver.navigate().back();
+
+        //6- Go to tesla.com again
+        driver.get("https://www.tesla.com");
+
+        //7- Stop code execution for 3 seconds again
+        Thread.sleep(3000);
+
+        //8- Use selenium to navigate refresh
+        driver.navigate().refresh();
+
+        //9- Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        //10- Use navigate().to():
+        driver.navigate().to("https://www.google.com");
+
+        //get the title of the page
+        //System.out.println("driver.getTitle(): "+driver.getTitle());
+        currentTitle = driver.getTitle();
+        System.out.println("currentTitle = " + currentTitle);
 
 
     }
