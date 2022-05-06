79518446fb8a660b0bbf087e5831a2cf60f2b482
YasinKaradag07 <yasinkaradag3607@gmail.com>
2022-05-06 16:09:08 +0300

    CydeoVerifications done

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -12,6 +12,25 @@ public class T1_CydeoVerifications {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        driver.navigate().to("https://practice.cydeo.com");
+        String expectedURL = "cydeo";
+        String actualURL = driver.getCurrentUrl();
+        if(actualURL.contains(expectedURL)){
+            System.out.println("URL verification PASSED!");
+        }else{
+            System.out.println("URL verification FAILED!");
+        }
+
+        String expectedTitle = "Practice";
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Title verification PASSED!");
+        }else{
+            System.out.println("Title verification FAILED!");
+        }
+
+        driver.close();
 
     }
 }
