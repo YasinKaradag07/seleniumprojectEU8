d1ce2a19c91715180da1af61b9955bcecbc63d32
YasinKaradag07 <yasinkaradag3607@gmail.com>
2022-05-13 14:34:26 +0300

    day4_task5_checkBoxes

---
+++ src/test/java/com/cydeo/tests/day4_findElements_chaeckBoxes_radio/T5_checkBoxes.java
@@ -0,0 +1,47 @@
+package com.cydeo.tests.day4_findElements_chaeckBoxes_radio;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class T5_checkBoxes {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cydeo.com/checkboxes");
+
+        WebElement checkBox1 = driver.findElement(By.xpath("//input[@name='checkbox1']"));
+        WebElement checkBox2 = driver.findElement(By.xpath("//input[@name='checkbox2']"));
+        System.out.println("checkBox1.isSelected() = " + checkBox1.isSelected());
+        System.out.println("checkBox2.isSelected() = " + checkBox2.isSelected());
+
+        Thread.sleep(2000);
+        checkBox1.click();
+
+        Thread.sleep(2000);
+        checkBox2.click();
+
+        System.out.println("checkBox1.isSelected() = " + checkBox1.isSelected());
+        System.out.println("checkBox2.isSelected() = " + checkBox2.isSelected());
+
+        driver.close();
+        driver.quit();
+
+
+    }
+}
+/*
+Practice: Checkboxes
+1. Go to http://practice.cydeo.com/checkboxes
+2. Confirm checkbox #1 is NOT selected by default 3. Confirm checkbox #2 is SELECTED by default.
+4. Click checkbox #1 to select it.
+5. Click checkbox #2 to deselect it.
+6. Confirm checkbox #1 is SELECTED.
+7. Confirm checkbox #2 is NOT selected.
+ */
