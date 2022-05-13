c3e54ff5c85bac68fece4755345c2d41a93df6e0
YasinKaradag07 <yasinkaradag3607@gmail.com>
2022-05-13 16:53:23 +0300

    day4_task3_homework xpath click button is completed

--- src/test/java/com/cydeo/tests/day4_findElements_chaeckBoxes_radio/T3_xpathPractice.java
+++ src/test/java/com/cydeo/tests/day4_findElements_chaeckBoxes_radio/T3_xpathPractice.java
@@ -1,10 +1,32 @@
 package com.cydeo.tests.day4_findElements_chaeckBoxes_radio;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
 public class T3_xpathPractice {
 
     public static void main(String[] args) {
 
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
 
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("http://practice.cydeo.com/multiple_buttons");
+        WebElement button1 = driver.findElement(By.xpath("//button[@onclick='button1()']"));
+        button1.click();
+        String actualResult = driver.findElement(By.xpath("//p[@id='result']")).getText();
+        if(actualResult.equals("Clicked on button one!")){
+            System.out.println("Result is as expected, test PASSED!");
+        }else{
+            System.out.println("Result is wrong, Test FAILED!");
+        }
+
+        driver.quit();
 
     }
 }
