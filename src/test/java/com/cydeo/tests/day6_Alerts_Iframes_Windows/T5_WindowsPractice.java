4a495064ef24f8f894d18c263f9030d2bb32938a
YasinKaradag07 <yasinkaradag3607@gmail.com>
2022-05-18 00:50:43 +0300

    T5_Windows practice

---
+++ src/test/java/com/cydeo/tests/day6_Alerts_Iframes_Windows/T5_WindowsPractice.java
@@ -0,0 +1,62 @@
+package com.cydeo.tests.day6_Alerts_Iframes_Windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T5_WindowsPractice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("https://practice.cydeo.com/windows");
+    }
+
+    @Test
+    public void multipleWindowTest(){
+        // Storing the main page's window handle as String is
+        // Good practice for future re-usable purposes
+        String mainHandle = driver.getWindowHandle();
+        System.out.println("mainHandle = " + mainHandle);
+
+        String expectedTitle = "Windows";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle,expectedTitle);
+        System.out.println("Title before click = " + actualTitle);
+
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+        clickHereLink.click();
+
+        for(String each : driver.getWindowHandles()){
+            driver.switchTo().window(each);
+            System.out.println("Current title while switching windows: " + driver.getTitle());
+        }
+
+        actualTitle = driver.getTitle();
+        System.out.println("Title after click = " + actualTitle);
+
+        driver.close();
+
+    }
+}
+/*
+TC #5: Window Handle practice
+1. Create a new class called: T5_WindowsPractice 2. Create new test and make set ups
+3. Go to : https://practice.cydeo.com/windows
+4. Assert: Title is “Windows”
+5. Click to: “Click Here” link
+6. Switch to new Window.
+7. Assert: Title is “New Window”
+ */
