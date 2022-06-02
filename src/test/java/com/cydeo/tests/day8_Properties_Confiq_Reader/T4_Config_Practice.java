33d333a400765a3de7905fbc6385814025df7196
YasinKaradag07 <yasinkaradag3607@gmail.com>
2022-06-02 13:44:00 +0300

    Javafaker, Singleton, TestBase

--- src/test/java/com/cydeo/tests/day8_Properties_Confiq_Reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day8_Properties_Confiq_Reader/T4_Config_Practice.java
@@ -11,29 +11,37 @@ import org.testng.annotations.Test;
 
 public class T4_Config_Practice {
 
-    public WebDriver driver;
-/*
-    @BeforeMethod
-    public void setupMethod(){
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.google.com");
-    }
-*/
+//    public WebDriver driver;
+//
+//    @BeforeMethod
+//    public void setupMethod(){
+//
+//        //We are getting the browserType dynamically from our configuration.properties file
+//        String browserType = ConfigurationReader.getProperty("browser");
+//        driver = WebDriverFactory.getDriver(browserType);
+//
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//
+//    }
+
     @Test
     public void google_search_test(){
+        //Driver.getDriver() --> driver
 
         Driver.getDriver().get("https://www.google.com");
 
-        WebElement googleSearchBox = driver.findElement(By.xpath("//input[@name='q']"));
+        //3- Write “apple” in search box
+        WebElement googleSearchBox = Driver.getDriver().findElement(By.xpath("//input[@name='q']"));
+
         googleSearchBox.sendKeys(ConfigurationReader.getProperty("searchValue") + Keys.ENTER);
 
-       // googleSearchBox.sendKeys("apple"+ Keys.ENTER);
-
+        //4- Verify title:
+        //Expected: apple - Google Search
         String expectedTitle = ConfigurationReader.getProperty("searchValue")+" - Google Search";
         String actualTitle = Driver.getDriver().getTitle();
-        Assert.assertEquals(actualTitle,expectedTitle);
+
+        Assert.assertEquals(actualTitle, expectedTitle);
     }
 
 
