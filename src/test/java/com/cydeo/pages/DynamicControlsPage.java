d1dbbb96af2405ab9e2901af90c8e34f8a470793
YasinKaradag07 <yasinkaradag3607@gmail.com>
2022-06-10 18:46:02 +0300

    Enable button test adde to ExplicitWaitPractices class

--- src/test/java/com/cydeo/pages/DynamicControlsPage.java
+++ src/test/java/com/cydeo/pages/DynamicControlsPage.java
@@ -24,5 +24,10 @@ public class DynamicControlsPage {
     @FindBy(css = "p[id='message']")
     public WebElement message;
 
+    @FindBy(xpath = "//button[.='Enable']")
+    public WebElement enableButton;
+
+    @FindBy(css = "input[type='text']")
+    public WebElement inputBox;
 
 }
--- src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
@@ -1,10 +1,9 @@
 package com.cydeo.tests.day13_review_and_practices;
 
 import com.cydeo.pages.DynamicControlsPage;
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -30,13 +29,11 @@ public class ExplicitWaitPractices {
 
         //4- Wait until “loading bar disappears”
         // In order to prevent conflicts and extra waiting time issues, we can turn off implicit wait
-        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
-        wait.until(ExpectedConditions.invisibilityOf(dynamicControlsPage.loadingBar));
-
+        BrowserUtils.waitForInvisibilityOf(dynamicControlsPage.loadingBar);
         //5- Verify:
         //a. Checkbox is not displayed
         try {
+            Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
             Assert.assertFalse(dynamicControlsPage.checkBox.isDisplayed());
         }catch(NoSuchElementException e){
             Assert.assertTrue(true);
@@ -47,4 +44,34 @@ public class ExplicitWaitPractices {
 
     }
 
+    @Test
+    public void enable_button_test(){
+        //3- Click to “Enable” button
+
+        //System.out.println("dynamicControlsPage.inputBox.isEnabled() = "
+        //        + dynamicControlsPage.inputBox.isEnabled());
+
+        dynamicControlsPage.enableButton.click();
+
+        System.out.println("dynamicControlsPage.inputBox.isEnabled() = "
+                + dynamicControlsPage.inputBox.isEnabled());
+
+        //4- Wait until “loading bar disappears”
+        //Calling our ExplicitWait utility method to wait loadingBar to disappear
+        BrowserUtils.waitForInvisibilityOf(dynamicControlsPage.loadingBar);
+
+        System.out.println("dynamicControlsPage.inputBox.isEnabled() = "
+                + dynamicControlsPage.inputBox.isEnabled());
+
+        //5- Verify:
+        //a. Input box is enabled.
+        Assert.assertTrue(dynamicControlsPage.inputBox.isEnabled());
+
+        //b. “It’s enabled!” message is displayed.
+        Assert.assertTrue(dynamicControlsPage.message.isDisplayed());
+
+        //Check the String value is matching as expected: “It’s enabled!”
+        Assert.assertTrue(dynamicControlsPage.message.getText().equals("It's enabled!"));
+
+    }
 }
