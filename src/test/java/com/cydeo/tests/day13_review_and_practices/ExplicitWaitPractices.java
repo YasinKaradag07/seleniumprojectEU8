377cc62a8265b8f3138ce4aad3b49930b9f7802b
YasinKaradag07 <yasinkaradag3607@gmail.com>
2022-06-10 16:11:50 +0300

    DoubleClick and ExplicitWait Practices

---
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
@@ -0,0 +1,50 @@
+package com.cydeo.tests.day13_review_and_practices;
+
+import com.cydeo.pages.DynamicControlsPage;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ExplicitWaitPractices {
+
+    DynamicControlsPage dynamicControlsPage;
+    @BeforeMethod
+    public void setupMethod(){
+        //TC #2: Explicit wait practice
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/dynamic_controls
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_controls");
+        dynamicControlsPage = new DynamicControlsPage();
+    }
+
+    @Test
+    public void remove_button_test(){
+        //3- Click to “Remove” button
+        dynamicControlsPage.removeButton.click();
+
+        //4- Wait until “loading bar disappears”
+        // In order to prevent conflicts and extra waiting time issues, we can turn off implicit wait
+        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.invisibilityOf(dynamicControlsPage.loadingBar));
+
+        //5- Verify:
+        //a. Checkbox is not displayed
+        try {
+            Assert.assertFalse(dynamicControlsPage.checkBox.isDisplayed());
+        }catch(NoSuchElementException e){
+            Assert.assertTrue(true);
+        }
+        //b. “It’s gone!” message is displayed.
+        //NOTE: FOLLOW POM
+        Assert.assertTrue(dynamicControlsPage.message.isDisplayed());
+
+    }
+
+}
